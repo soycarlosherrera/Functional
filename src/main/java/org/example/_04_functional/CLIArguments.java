57cf8d82fb21b14b8292148ec6d0153a6a78566f
Carlos Herrera <carlos4505@gmail.com>
2023-06-06 10:38:39 -0500

    Consumer and Supplier

---
+++ src/main/java/org/example/_04_functional/CLIArguments.java
@@ -0,0 +1,10 @@
+package org.example._04_functional;
+
+public class CLIArguments {
+
+    private boolean isHelp;
+
+    public boolean isHelp(){
+        return isHelp;
+    }
+}
---
+++ src/main/java/org/example/_04_functional/CLIArgumentsUtils.java
@@ -0,0 +1,24 @@
+package org.example._04_functional;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class CLIArgumentsUtils {
+    static void showHelp(CLIArguments cliArguments){
+        Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
+            if (cliArguments1.isHelp())
+                System.out.println("Manual Solicitado");
+        };
+
+        consumerHelper.accept(cliArguments);
+
+    }
+
+    static CLIArguments generateCLI(){
+        Supplier<CLIArguments> generator = () -> new CLIArguments();
+
+        return generator.get();
+
+    }
+
+}
