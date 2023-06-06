a156ec172d00ff7f9f0e690004867f4b20ccf0de
Carlos Herrera <carlos4505@gmail.com>
2023-06-06 09:04:54 -0500

    Predicate

--- src/main/java/org/example/_04_functional/MathFunctions.java
+++ src/main/java/org/example/_04_functional/MathFunctions.java
@@ -1,6 +1,7 @@
 package org.example._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
 
@@ -17,6 +18,29 @@ public class MathFunctions {
 
         System.out.println(square(5));
 
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1 ;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+        isEven.test(4); //true
+
+        Predicate<Student> isAproved = student -> student.getCalificacion() >= 6.0;
+
+        Student carlos = new Student(5.9);
+        System.out.println(isAproved.test(carlos));
+
+    }
+
+    static class Student{
+        private double calificacion;
+
+        public Student(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
     }
 
     static int square(int x){
