837f04272fd6c34fce73f614cfe3a06acf162165
Carlos Herrera <carlos4505@gmail.com>
2023-06-08 11:44:36 -0500

    Default

---
+++ src/main/java/org/example/_09_defaults/StringFunction.java
@@ -0,0 +1,34 @@
+package org.example._09_defaults;
+
+public class StringFunction {
+    @FunctionalInterface
+    interface StringOperation {
+        int getAmount();
+
+        default void operate(String text) {
+            int x = getAmount();
+            while(x-- > 0){
+                System.out.println(text);
+            }
+        }
+    }
+
+    @FunctionalInterface
+    interface DoOperation {
+        void take(String text);
+
+        default void execute(int x, String text){
+            while(x-- > 0){
+                take(text);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        StringOperation six = () -> 6;
+        six.operate("Alumno");
+
+        DoOperation operateFive = text -> System.out.println(text);
+        operateFive.execute(5,"Platzi");
+    }
+}
