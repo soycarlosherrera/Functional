411728095387f1c305231c3374800988a55d1b06
Carlos Herrera <carlos4505@gmail.com>
2023-06-11 17:43:20 -0500

    Operaciones Terminales e intermedias

--- src/main/java/org/example/_13_streams/TypeStream.java
+++ src/main/java/org/example/_13_streams/TypeStream.java
@@ -1,5 +1,7 @@
 package org.example._13_streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
@@ -9,5 +11,11 @@ public class TypeStream {
                 .parallel()
                 .filter(x -> x % 2 == 0)
                 .forEach(System.out::println);
+
+        IntStream infiniteStream2 = IntStream.iterate(0, x -> x + 1);
+        List<Integer> numberList = infiniteStream2.limit(1000)
+                .filter(x -> x % 2 == 0)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
